0f14f76d32b9ecd9f52367dc76356878a5aa00cb
Lyra7770 <lyramir955@gmail.com>
2025-06-24 00:50:57 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,18 +1,18 @@
 let cesped = [];
-let separacion = 12;
+let separacion = 14;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  pixelDensity(1); // evita sobrecarga en móviles
+  pixelDensity(1);
   crearCesped();
 }
 
 function draw() {
-  dibujarFondo();
+  fondoGradiente();
 
-  for (let i = 0; i < cesped.length; i++) {
-    cesped[i].mover(mouseX, mouseY);
-    cesped[i].dibujar();
+  for (let brizna of cesped) {
+    brizna.actualizar();
+    brizna.dibujar();
   }
 }
 
@@ -30,50 +30,11 @@ function crearCesped() {
   }
 }
 
-function dibujarFondo() {
+function fondoGradiente() {
   noStroke();
   for (let y = 0; y < height; y++) {
-    let c = lerpColor(color(192, 235, 192), color(162, 210, 162), y / height);
+    let c = lerpColor(color(210, 245, 210), color(170, 220, 170), y / height);
     stroke(c);
     line(0, y, width, y);
-  }
-}
-
-class Brizna {
-  constructor(x, y) {
-    this.x = x;
-    this.y = y;
-    this.altura = random(20, 42);
-    this.inclinacion = 0;
-    this.angulo = random(TWO_PI);
-    let tonoVerde = random(100, 180);
-    this.color = color(40, tonoVerde, 70, 180);
-  }
-
-  mover(mx, my) {
-    this.angulo += 0.02;
-    let viento = sin(this.angulo) * 1.3;
-
-    let d = dist(this.x, this.y, mx, my);
-    let efecto = (d < 140) ? map(d, 0, 140, 4, 0) : 0;
-
-    this.inclinacion = viento + efecto;
-  }
-
-  dibujar() {
-    stroke(this.color);
-    strokeWeight(1.1);
-    noFill();
-
-    let x1 = this.x;
-    let y1 = this.y;
-    let x2 = x1 + this.inclinacion * 0.3;
-    let y2 = y1 - this.altura * 0.3;
-    let x3 = x1 + this.inclinacion * 0.7;
-    let y3 = y1 - this.altura * 0.7;
-    let x4 = x1 + this.inclinacion;
-    let y4 = y1 - this.altura;
-
-    bezier(x1, y1, x2, y2, x3, y3, x4, y4);
   }
 }
