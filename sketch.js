6e0dd3d4ec4ac0ccebf176a5b1e609b91a15482d
Lyra7770 <lyramir955@gmail.com>
2025-06-24 00:55:40 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,27 +1,26 @@
 let cesped = [];
-let separacion = 14;
+let separacion = 12;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   pixelDensity(1);
-  crearCesped();
-}
-
-function draw() {
-  fondoGradiente();
-
-  for (let brizna of cesped) {
-    brizna.actualizar();
-    brizna.dibujar();
-  }
+  initCesped();
 }
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
-  crearCesped();
+  initCesped();
 }
 
-function crearCesped() {
+function draw() {
+  dibujarFondo();
+  for (let b of cesped) {
+    b.mover(mouseX, mouseY);
+    b.dibujar();
+  }
+}
+
+function initCesped() {
   cesped = [];
   for (let x = 0; x < width; x += separacion) {
     for (let y = height * 0.5; y < height; y += separacion) {
@@ -30,10 +29,10 @@ function crearCesped() {
   }
 }
 
-function fondoGradiente() {
-  noStroke();
+function dibujarFondo() {
+  noFill();
   for (let y = 0; y < height; y++) {
-    let c = lerpColor(color(210, 245, 210), color(170, 220, 170), y / height);
+    let c = lerpColor(color(210,245,210), color(170,220,170), y/height);
     stroke(c);
     line(0, y, width, y);
   }
@@ -41,37 +40,33 @@ function fondoGradiente() {
 
 class Brizna {
   constructor(x, y) {
-    this.x = x;
-    this.y = y;
+    this.x = x; this.y = y;
     this.altura = random(20, 40);
-    this.curva = random(0.5, 1.2);
-    this.oscilacion = random(0.5, 1.2);
     this.fase = random(TWO_PI);
-    this.tiempo = random(1000);
-
-    let verde = random(100, 180);
-    this.color = color(40, verde, 60, 180);
+    this.vel = random(0.008, 0.02);
+    this.curva = random(0.8, 1.4);
+    this.t = random(1000);
+    let v = random(100,180);
+    this.color = color(40, v, 70, 200);
   }
 
-  actualizar() {
-    this.tiempo += 0.015; // Movimiento constante
+  mover(mx, my) {
+    this.t += this.vel;
+    this.osc = sin(this.t + this.fase) * this.curva;
+    if (dist(this.x, this.y, mx, my) < 100) {
+      this.osc += map(dist(this.x, this.y, mx, my), 0, 100, 2, 0);
+    }
   }
 
   dibujar() {
-    let osc = sin(this.tiempo + this.fase) * this.curva;
-
-    let x1 = this.x;
-    let y1 = this.y;
-    let x2 = x1 + osc * 0.5;
-    let y2 = y1 - this.altura * 0.3;
-    let x3 = x1 + osc * 1.1;
-    let y3 = y1 - this.altura * 0.7;
-    let x4 = x1 + osc * 1.6;
-    let y4 = y1 - this.altura;
-
     stroke(this.color);
     strokeWeight(1);
     noFill();
-    bezier(x1, y1, x2, y2, x3, y3, x4, y4);
+
+    let x1=this.x, y1=this.y;
+    let x2=x1 + this.osc*0.5, y2=y1 - this.altura*0.3;
+    let x3=x1 + this.osc*0.8, y3=y1 - this.altura*0.7;
+    let x4=x1 + this.osc, y4=y1 - this.altura;
+    bezier(x1,y1,x2,y2,x3,y3,x4,y4);
   }
 }
