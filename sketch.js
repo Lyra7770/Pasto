f4f4894f78e72cf883b101bb34b9e0c77856fe4f
Lyra7770 <lyramir955@gmail.com>
2025-06-24 00:51:25 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -38,3 +38,40 @@ function fondoGradiente() {
     line(0, y, width, y);
   }
 }
+
+class Brizna {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.altura = random(20, 40);
+    this.curva = random(0.5, 1.2);
+    this.oscilacion = random(0.5, 1.2);
+    this.fase = random(TWO_PI);
+    this.tiempo = random(1000);
+
+    let verde = random(100, 180);
+    this.color = color(40, verde, 60, 180);
+  }
+
+  actualizar() {
+    this.tiempo += 0.015; // Movimiento constante
+  }
+
+  dibujar() {
+    let osc = sin(this.tiempo + this.fase) * this.curva;
+
+    let x1 = this.x;
+    let y1 = this.y;
+    let x2 = x1 + osc * 0.5;
+    let y2 = y1 - this.altura * 0.3;
+    let x3 = x1 + osc * 1.1;
+    let y3 = y1 - this.altura * 0.7;
+    let x4 = x1 + osc * 1.6;
+    let y4 = y1 - this.altura;
+
+    stroke(this.color);
+    strokeWeight(1);
+    noFill();
+    bezier(x1, y1, x2, y2, x3, y3, x4, y4);
+  }
+}
